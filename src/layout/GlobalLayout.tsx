53b966ef15dac40acfe217669fd604a77e1f0c48
Arkar Soe <arkarsoedev@gmail.com>
2025-10-04 17:48:15 +0630

    feat: remove pwa one click testing

--- src/layout/GlobalLayout.tsx
+++ src/layout/GlobalLayout.tsx
@@ -1,39 +1,36 @@
-// import { useEffect, useState } from "react"
-// import { Outlet, useNavigate, useSearchParams } from "react-router-dom";
-// import { decryptToken } from "../util";
-// import { useDispatch } from "react-redux";
-// import { fetchUserFromToken } from "../store/authSlice";
-// import Cookies from "js-cookie";
-
-import { useState } from "react"
-import { Outlet } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Outlet, useNavigate, useSearchParams } from "react-router-dom";
+import { decryptToken } from "../util";
+import { useDispatch } from "react-redux";
+import { fetchUserFromToken } from "../store/authSlice";
+import Cookies from "js-cookie";
 
 const GlobalLayout = () => {
-  // const dispatch = useDispatch();
-  // const [searchParams] = useSearchParams();
-  // const token = searchParams.get("token");
-  // const navigate = useNavigate();
-  // const key = import.meta.env.VITE_BASE_ENCRYPTION_KEY;
-  // const isAuthenticated = Cookies.get("isAuthenticated");
-  const [authenticating, _setAuthenticating] = useState(true)
+  const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
+  const navigate = useNavigate();
+  const key = import.meta.env.VITE_BASE_ENCRYPTION_KEY;
+  const isAuthenticated = Cookies.get("isAuthenticated");
+  const [authenticating, setAuthenticating] = useState(true)
 
-  // useEffect(() => {
-  //   if (!isAuthenticated) {
-  //     if (token) {
-  //       const data = decryptToken(token, key)
-  //       dispatch(fetchUserFromToken({
-  //         data: JSON.parse(data)
-  //       }) as any).then(() => {
-  //         navigate('/')
-  //         setAuthenticating(false)
-  //       })
-  //     } else {
-  //       window.location.href = import.meta.env.VITE_BASE_AUTH_URL
-  //     }
-  //   } else {
-  //     setAuthenticating(false)
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (!isAuthenticated) {
+      if (token) {
+        const data = decryptToken(token, key)
+        dispatch(fetchUserFromToken({
+          data: JSON.parse(data)
+        }) as any).then(() => {
+          navigate('/')
+          setAuthenticating(false)
+        })
+      } else {
+        window.location.href = import.meta.env.VITE_BASE_AUTH_URL
+      }
+    } else {
+      setAuthenticating(false)
+    }
+  }, []);
 
   return (
     <>
